2dfa6a89ae4ac5af9e1bf2fcad1a015dc5e6ec4b
MishaNya <kholodm07@gmail.com>
2021-06-04 16:11:47 +0300

    lw8
    reupload

--- Lw8/task8.js
+++ Lw8/task8.js
@@ -1,48 +1,69 @@
-let firstNumber
-let secondNumber
-let operations = ['*', '/', '+', '-']
+tests();
 
-calc('*(- 5 6) 7');
-calc('+ 5 6');
+function tests() {
+    calc('*(- 5 6) 7');
+    calc('- 5 6');
+    calc('+ + 6 5');
+    calc(' 5 6');
+    calc(' 6');
+    calc('+ 6');
+    calc('asdasdasdasd');
+    calc('');
+}
 
-function calc(x) {
-    console.log(x)
-    firstNumber = null
-    secondNumber = null
-    let thisNumber = null
-    let myOperations = []
-    for (let i = 0; i < x.length; i++) {
-        if (operations.indexOf(x[i]) != -1) {
-            myOperations.push(x[i])
+function calc(number) {
+    console.log(number);
+    const operations = ['*', '/', '+', '-'];
+    let errorFlag = false;
+    let firstNumber = null;
+    let secondNumber = null;
+    let thisNumber = null;
+    let myOperations = [];
+
+    for (let i = 0; i < number.length; i++) {
+
+        if (operations.indexOf(number[i]) != -1) {
+            myOperations.push(number[i]);
+            continue;
         }
-        thisNumber = parseInt(x[i])
+
+        thisNumber = parseInt(number[i]);
+
         if (thisNumber <= 9 && thisNumber >= 0) {
+
             if (firstNumber == null) {
-                firstNumber = thisNumber
+                firstNumber = thisNumber;
             } else {
-                secondNumber = thisNumber
-                firstNumber = doCalc(myOperations.pop(), firstNumber, secondNumber)
+                secondNumber = thisNumber;
+                firstNumber = doCalc(myOperations.pop(), firstNumber, secondNumber);
             }
+
         }
     }
-    console.log(firstNumber);
+
+    if (myOperations.length != 0 || secondNumber == null || typeof firstNumber == 'undefined') {
+        errorFlag = true;
+    }
+
+    if (errorFlag) {
+        console.log('Wrong example');
+    } else {
+        console.log(firstNumber);
+    }
+
 }
 
 function doCalc(oper, numb1, numb2) {
     switch (oper) {
         case '*':
-            console.log(numb1 + '*' + numb2)
-            return numb1 * numb2
+            return numb1 * numb2;
         case '/':
-            console.log(numb1 + '/' + numb2)
-            return numb1 / numb2
+            return numb1 / numb2;
         case '+':
-            console.log(numb1 + '+' + numb2)
-            return numb1 + numb2
+            return numb1 + numb2;
         case '-':
-            console.log(numb1 + '-' + numb2)
-            return numb1 - numb2
+            return numb1 - numb2;
         default:
-            console.log('Error operation')
+            errorFlag = true;
     }
 }
