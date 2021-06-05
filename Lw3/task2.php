2a535572a0d77dd6afdfaee8ffbd885d3acd637a
MishaNya <kholodm07@gmail.com>
2021-06-05 14:31:14 +0300

    lw3
    Task2 fix

--- Lw3/task2.php
+++ Lw3/task2.php
@@ -1,25 +1,25 @@
 <?php
 header("Content-Type: text/plain");
 
-if (!isset($_GET['identifier']))   // проверка наличия переменной в queary string
+if (!isset($_GET['identifier']))
 {
-    die("Set identifier");  // если нет, то завершить прогу
+    die("set identifier");
 }
 
-$checkString = $_GET['identifier'];  
+$checkString = $_GET['identifier'];
 
 for ($i = 0; $i < strlen($checkString); $i++)
 {
     if($i == 0)
     {
-        if(!preg_match("/[a-zа-я]/i", $checkString))
+        if(ctype_digit($checkString[$i]))
         {
-            die("No, first symbol must be letter");
+            die("no, first symbol must be letter");
         }
     }
     if(!ctype_alnum($checkString[$i]))
     {
-        die("No, only letter and digit");
+        die("no, only letter and digit");
     }
 }
 
