4ad91b246cc1c1fbedd9eeadd0cefbdcf1c9c37a
MishaNya <kholodm07@gmail.com>
2021-06-05 21:58:56 +0300

    lw11
    Table names fix

--- lw11/task1.sql
+++ lw11/task1.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE lw11;
 USE lw11;
 
-CREATE TABLE faculties
+CREATE TABLE faculty
 (
     id                INT AUTO_INCREMENT NOT NULL,
     faculty           VARCHAR(255)       NOT NULL,
@@ -10,7 +10,7 @@ CREATE TABLE faculties
   COLLATE `utf8mb4_unicode_ci`
   ENGINE = InnoDB;
 
-CREATE TABLE groups
+CREATE TABLE gruppa
 (
     id                INT AUTO_INCREMENT NOT NULL,
     `group`           VARCHAR(255)       NOT NULL,
@@ -20,7 +20,7 @@ CREATE TABLE groups
   COLLATE `utf8mb4_unicode_ci`
   ENGINE = InnoDB;
 
-CREATE TABLE students
+CREATE TABLE student
 (
     id                INT AUTO_INCREMENT NOT NULL,
     first_name        VARCHAR(255)       NOT NULL,
@@ -32,116 +32,116 @@ CREATE TABLE students
   COLLATE `utf8mb4_unicode_ci`
   ENGINE = InnoDB;
 
-INSERT INTO faculties SET faculty = 'PS';
-INSERT INTO faculties SET faculty = 'BI';
-INSERT INTO faculties SET faculty = 'ISA';
+INSERT INTO faculty SET faculty = 'PS';
+INSERT INTO faculty SET faculty = 'BI';
+INSERT INTO faculty SET faculty = 'ISA';
 
-INSERT INTO groups SET `group` = 'PS-11', faculty_id = 1;
-INSERT INTO groups SET `group` = 'PS-12', faculty_id = 1;
-INSERT INTO groups SET `group` = 'PS-13', faculty_id = 1;
+INSERT INTO gruppa SET `group` = 'PS-11', faculty_id = 1;
+INSERT INTO gruppa SET `group` = 'PS-12', faculty_id = 1;
+INSERT INTO gruppa SET `group` = 'PS-13', faculty_id = 1;
 
-INSERT INTO groups SET `group` = 'BI-11', faculty_id = 2;
-INSERT INTO groups SET `group` = 'BI-12', faculty_id = 2;
-INSERT INTO groups SET `group` = 'BI-13', faculty_id = 2;
+INSERT INTO gruppa SET `group` = 'BI-11', faculty_id = 2;
+INSERT INTO gruppa SET `group` = 'BI-12', faculty_id = 2;
+INSERT INTO gruppa SET `group` = 'BI-13', faculty_id = 2;
 
-INSERT INTO groups SET `group` = 'ISA-11', faculty_id = 3;
-INSERT INTO groups SET `group` = 'ISA-12', faculty_id = 3;
-INSERT INTO groups SET `group` = 'ISA-13', faculty_id = 3;
+INSERT INTO gruppa SET `group` = 'ISA-11', faculty_id = 3;
+INSERT INTO gruppa SET `group` = 'ISA-12', faculty_id = 3;
+INSERT INTO gruppa SET `group` = 'ISA-13', faculty_id = 3;
 
-INSERT INTO students SET first_name = 'A',  last_name = 'a', age = 19, group_id = 1;
-INSERT INTO students SET first_name = 'B',  last_name = 'b', age = 20, group_id = 2;
-INSERT INTO students SET first_name = 'C',  last_name = 'c', age = 18, group_id = 3;
-INSERT INTO students SET first_name = 'D',  last_name = 'd', age = 21, group_id = 4;
-INSERT INTO students SET first_name = 'E',  last_name = 'e', age = 20, group_id = 5;
+INSERT INTO student SET first_name = 'A',  last_name = 'a', age = 19, group_id = 1;
+INSERT INTO student SET first_name = 'B',  last_name = 'b', age = 20, group_id = 2;
+INSERT INTO student SET first_name = 'C',  last_name = 'c', age = 18, group_id = 3;
+INSERT INTO student SET first_name = 'D',  last_name = 'd', age = 21, group_id = 4;
+INSERT INTO student SET first_name = 'E',  last_name = 'e', age = 20, group_id = 5;
 
-INSERT INTO students SET first_name = 'A',  last_name = 'a', age = 19, group_id = 6;
-INSERT INTO students SET first_name = 'B',  last_name = 'b', age = 20, group_id = 7;
-INSERT INTO students SET first_name = 'C',  last_name = 'c', age = 18, group_id = 8;
-INSERT INTO students SET first_name = 'D',  last_name = 'd', age = 21, group_id = 9;
-INSERT INTO students SET first_name = 'E',  last_name = 'e', age = 20, group_id = 1;
+INSERT INTO student SET first_name = 'A',  last_name = 'a', age = 19, group_id = 6;
+INSERT INTO student SET first_name = 'B',  last_name = 'b', age = 20, group_id = 7;
+INSERT INTO student SET first_name = 'C',  last_name = 'c', age = 18, group_id = 8;
+INSERT INTO student SET first_name = 'D',  last_name = 'd', age = 21, group_id = 9;
+INSERT INTO student SET first_name = 'E',  last_name = 'e', age = 20, group_id = 1;
 
-INSERT INTO students SET first_name = 'A',  last_name = 'a', age = 19, group_id = 2;
-INSERT INTO students SET first_name = 'B',  last_name = 'b', age = 20, group_id = 3;
-INSERT INTO students SET first_name = 'C',  last_name = 'c', age = 18, group_id = 4;
-INSERT INTO students SET first_name = 'D',  last_name = 'd', age = 21, group_id = 5;
-INSERT INTO students SET first_name = 'E',  last_name = 'e', age = 20, group_id = 6;
+INSERT INTO student SET first_name = 'A',  last_name = 'a', age = 19, group_id = 2;
+INSERT INTO student SET first_name = 'B',  last_name = 'b', age = 20, group_id = 3;
+INSERT INTO student SET first_name = 'C',  last_name = 'c', age = 18, group_id = 4;
+INSERT INTO student SET first_name = 'D',  last_name = 'd', age = 21, group_id = 5;
+INSERT INTO student SET first_name = 'E',  last_name = 'e', age = 20, group_id = 6;
 
-INSERT INTO students SET first_name = 'A',  last_name = 'a', age = 19, group_id = 7;
-INSERT INTO students SET first_name = 'B',  last_name = 'b', age = 20, group_id = 8;
-INSERT INTO students SET first_name = 'C',  last_name = 'c', age = 18, group_id = 9;
-INSERT INTO students SET first_name = 'D',  last_name = 'd', age = 21, group_id = 1;
-INSERT INTO students SET first_name = 'E',  last_name = 'e', age = 20, group_id = 2;
+INSERT INTO student SET first_name = 'A',  last_name = 'a', age = 19, group_id = 7;
+INSERT INTO student SET first_name = 'B',  last_name = 'b', age = 20, group_id = 8;
+INSERT INTO student SET first_name = 'C',  last_name = 'c', age = 18, group_id = 9;
+INSERT INTO student SET first_name = 'D',  last_name = 'd', age = 21, group_id = 1;
+INSERT INTO student SET first_name = 'E',  last_name = 'e', age = 20, group_id = 2;
 
-INSERT INTO students SET first_name = 'A',  last_name = 'a', age = 19, group_id = 3;
-INSERT INTO students SET first_name = 'B',  last_name = 'b', age = 20, group_id = 4;
-INSERT INTO students SET first_name = 'C',  last_name = 'c', age = 18, group_id = 5;
-INSERT INTO students SET first_name = 'D',  last_name = 'd', age = 21, group_id = 6;
-INSERT INTO students SET first_name = 'E',  last_name = 'e', age = 20, group_id = 7;
+INSERT INTO student SET first_name = 'A',  last_name = 'a', age = 19, group_id = 3;
+INSERT INTO student SET first_name = 'B',  last_name = 'b', age = 20, group_id = 4;
+INSERT INTO student SET first_name = 'C',  last_name = 'c', age = 18, group_id = 5;
+INSERT INTO student SET first_name = 'D',  last_name = 'd', age = 21, group_id = 6;
+INSERT INTO student SET first_name = 'E',  last_name = 'e', age = 20, group_id = 7;
 
-INSERT INTO students SET first_name = 'A',  last_name = 'a', age = 19, group_id = 8;
-INSERT INTO students SET first_name = 'B',  last_name = 'b', age = 20, group_id = 9;
-INSERT INTO students SET first_name = 'C',  last_name = 'c', age = 18, group_id = 1;
-INSERT INTO students SET first_name = 'D',  last_name = 'd', age = 21, group_id = 2;
-INSERT INTO students SET first_name = 'E',  last_name = 'e', age = 20, group_id = 3;
+INSERT INTO student SET first_name = 'A',  last_name = 'a', age = 19, group_id = 8;
+INSERT INTO student SET first_name = 'B',  last_name = 'b', age = 20, group_id = 9;
+INSERT INTO student SET first_name = 'C',  last_name = 'c', age = 18, group_id = 1;
+INSERT INTO student SET first_name = 'D',  last_name = 'd', age = 21, group_id = 2;
+INSERT INTO student SET first_name = 'E',  last_name = 'e', age = 20, group_id = 3;
 
-INSERT INTO students SET first_name = 'A',  last_name = 'a', age = 19, group_id = 4;
-INSERT INTO students SET first_name = 'B',  last_name = 'b', age = 20, group_id = 5;
-INSERT INTO students SET first_name = 'C',  last_name = 'c', age = 18, group_id = 6;
-INSERT INTO students SET first_name = 'D',  last_name = 'd', age = 21, group_id = 7;
-INSERT INTO students SET first_name = 'E',  last_name = 'e', age = 20, group_id = 8;
+INSERT INTO student SET first_name = 'A',  last_name = 'a', age = 19, group_id = 4;
+INSERT INTO student SET first_name = 'B',  last_name = 'b', age = 20, group_id = 5;
+INSERT INTO student SET first_name = 'C',  last_name = 'c', age = 18, group_id = 6;
+INSERT INTO student SET first_name = 'D',  last_name = 'd', age = 21, group_id = 7;
+INSERT INTO student SET first_name = 'E',  last_name = 'e', age = 20, group_id = 8;
 
-INSERT INTO students SET first_name = 'A',  last_name = 'a', age = 19, group_id = 9;
-INSERT INTO students SET first_name = 'B',  last_name = 'b', age = 20, group_id = 1;
-INSERT INTO students SET first_name = 'C',  last_name = 'c', age = 18, group_id = 2;
-INSERT INTO students SET first_name = 'D',  last_name = 'd', age = 21, group_id = 3;
-INSERT INTO students SET first_name = 'E',  last_name = 'e', age = 20, group_id = 4;
+INSERT INTO student SET first_name = 'A',  last_name = 'a', age = 19, group_id = 9;
+INSERT INTO student SET first_name = 'B',  last_name = 'b', age = 20, group_id = 1;
+INSERT INTO student SET first_name = 'C',  last_name = 'c', age = 18, group_id = 2;
+INSERT INTO student SET first_name = 'D',  last_name = 'd', age = 21, group_id = 3;
+INSERT INTO student SET first_name = 'E',  last_name = 'e', age = 20, group_id = 4;
 
-INSERT INTO students SET first_name = 'A',  last_name = 'a', age = 19, group_id = 5;
-INSERT INTO students SET first_name = 'B',  last_name = 'b', age = 20, group_id = 6;
-INSERT INTO students SET first_name = 'C',  last_name = 'c', age = 18, group_id = 7;
-INSERT INTO students SET first_name = 'D',  last_name = 'd', age = 21, group_id = 8;
-INSERT INTO students SET first_name = 'E',  last_name = 'e', age = 20, group_id = 9;
+INSERT INTO student SET first_name = 'A',  last_name = 'a', age = 19, group_id = 5;
+INSERT INTO student SET first_name = 'B',  last_name = 'b', age = 20, group_id = 6;
+INSERT INTO student SET first_name = 'C',  last_name = 'c', age = 18, group_id = 7;
+INSERT INTO student SET first_name = 'D',  last_name = 'd', age = 21, group_id = 8;
+INSERT INTO student SET first_name = 'E',  last_name = 'e', age = 20, group_id = 9;
 
 SELECT
     first_name,
-    last_name AS family_name,
-    age AS Age
+    last_name
+    age
 FROM
-    students
+    student
 WHERE
-    students.age = 20;
+    student.age = 20;
 
 SELECT
     first_name,
-    last_name AS family_name,
-    age AS Age,
+    last_name
+    age,
     `group`
 FROM 
-    students
-    JOIN groups ON students.group_id = groups.id
+    student
+    LEFT JOIN gruppa ON student.group_id = gruppa.id
 WHERE
     group_id = 7;
     
 SELECT
     first_name,
-    last_name AS family_name,
+    last_name
     faculty
 FROM 
-    students
-    JOIN groups ON students.group_id = groups.id 
-    JOIN faculties ON groups.faculty_id = faculties.id   
+    student
+    LEFT JOIN gruppa ON student.group_id = gruppa.id 
+    LEFT JOIN faculty ON gruppa.faculty_id = faculty.id   
 WHERE
     faculty_id = 1;
     
 SELECT
     first_name,
-    last_name AS family_name,
+    last_name
     age,
     `group`,
     faculty
 FROM
-    students
-    JOIN groups ON students.group_id = groups.id
-    JOIN faculties ON groups.faculty_id = faculties.id
+    student
+    LEFT JOIN gruppa ON student.group_id = gruppa.id
+    LEFT JOIN faculty ON gruppa.faculty_id = faculty.id
 WHERE
-    students.id = 40;
+    student.id = 40;
